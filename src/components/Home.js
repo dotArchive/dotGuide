a5290e87c548cdc59078064a7f5d8185a91bd08e
Edwin Detrez <edetrezjr@gmail.com>
2022-03-22 16:25:10 -0400

    Homepage setup - WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React from "react";
 
 export const Home = () => {
-	return (
-	<div id="home">
-		<div>Search Bar</div>
-		<div>About</div>
-		<div>README Categories</div>
-		<div>New README</div>
-		<div>
-			<ul>
-				<li>
-					LATEST README
-				</li>
-			</ul>
-		</div>
-	</div>
-	)
+  return (
+    <div id="home">
+      <div>
+        <div>About:</div>
+        <div>
+          You really think you can fly that thing? What do they got in there?
+          King Kong? Must go faster... go, go, go, go, go! You're a very
+          talented young man, with your own clever thoughts and ideas. Do you
+          need a manager? Hey, take a look at the earthlings. Goodbye! Remind me
+          to thank John for a lovely weekend. Yeah, but your scientists were so
+          preoccupied with whether or not they could, they didn't stop to think
+          if they should. Just my luck, no ice. Remind me to thank John for a
+          lovely weekend. They're using our own satellites against us. And the
+          clock is ticking.
+        </div>
+      </div>
+      <div>README Categories</div>
+      <div>New README</div>
+      <div>
+        <ul>
+          <li>LATEST README</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
