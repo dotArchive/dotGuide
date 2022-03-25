6d737544fd1f8fe6d435f9658c0841408b2180f8
JollyRen <jollyren@gmail.com>
2022-03-25 17:05:48 +0100

    implemented changes from group meeting this morning

--- src/components/Guide/SingleGuide/SingleGuide.js
+++ src/components/Guide/SingleGuide/SingleGuide.js
@@ -7,22 +7,22 @@ import { Typography, Box, Button, Card, Container } from '@mui/material'
 export default function SingleGuide() {
   let { guideId } = useParams()
   const [guide, setGuide] = useState({})
-  // const [time, setTime] = useState('')
-  const { frontEnd, backEnd, tags, apis, languages, username } = guide
+  const { frontEnd, backEnd, tags, apis, languages, username, title, createdAt, description } =
+    guide
 
   const getGuide = async () => {
     const docSnap = await getDoc(doc(db, 'guides', guideId))
     if (docSnap.exists()) {
       setGuide(docSnap.data())
     } else {
-      console.log(`doesn't work foo!`)
+      console.log(`unable to get guide!`)
     }
   }
   useEffect(() => {
     return getGuide()
   }, [])
 
-  return guide.createdAt ? (
+  return Object.keys(guide).length ? (
     <Container
       sx={{
         display: 'flex',
@@ -31,17 +31,47 @@ export default function SingleGuide() {
         justifyContent: 'center',
       }}>
       {console.log(guide)}
-      <Typography variant="h3" sx={{ color: 'white' }}>
-        {guide.title}
+      {/*
+         Title
+      */}
+
+      <Typography variant="h3" sx={{ color: 'white', ml: 1 }}>
+        {title.toUpperCase()}
+
+        {/*
+          top card
+      */}
       </Typography>
-      <Card sx={{ background: '#2f2f2f', p: 1, pl: 2, pr: 2 }}>
+      <Card
+        sx={{ background: '#2f2f2f', p: 1, pl: 2, pr: 2, border: 1.25, borderColor: '#353540' }}>
+        {/*
+          timestamp and username logic
+      */}
         <Typography sx={{ color: 'white', fontSize: '0.75em' }}>
-          {`${username} — ${guide.createdAt.toDate().toString().slice(0, 25)}`}
+          {`${username} — ${createdAt.toDate().toString().slice(0, 25)}`}
         </Typography>
-        <Typography sx={{ color: 'white' }}>{guide.description}</Typography>
+        {/*
+
+      */}
+        <Typography sx={{ color: 'white' }}>{description}</Typography>
       </Card>
-      <Typography sx={{ color: 'white' }}>Technologies Used</Typography>
-      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+
+      {/*
+          technologies used begin here
+      */}
+
+      <Typography sx={{ color: 'white', ml: 1 }}>Technologies Used</Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          // flexDirection: 'row',
+          justifyContent: 'space-between',
+          // alignContent: 'center',
+        }}>
+        {/*
+          start the tech cards here
+      */}
+
         <Card
           sx={{
             background: '#2f2f2f',
@@ -52,11 +82,13 @@ export default function SingleGuide() {
             width: '25%',
             minHeight: '10vh',
             textOverflow: 'ellipsis',
+            border: 1.25,
+            borderColor: '#353540',
           }}>
           <Typography sx={{ color: 'white' }} gutterBottom>
             Languages
           </Typography>
-          {languages ? (
+          {languages.length ? (
             languages.map((item, idx) => {
               return (
                 <Typography
@@ -64,14 +96,16 @@ export default function SingleGuide() {
                   sx={{
                     color: 'white',
                     fontSize: '0.7em',
+                    minHeight: 18,
                     p: 0.5,
                     mt: 0.5,
                     mb: 0.5,
                     border: 1,
                     borderColor: 'white',
                     borderRadius: 3,
+                    textAlign: 'center',
                   }}>
-                  {`${item.frontEnd}`}
+                  {`${item}`}
                 </Typography>
               )
             })
@@ -80,17 +114,20 @@ export default function SingleGuide() {
               sx={{
                 color: 'white',
                 fontSize: '0.7em',
+                minHeight: 18,
                 p: 0.5,
                 mt: 0.5,
                 mb: 0.5,
                 border: 1,
                 borderColor: 'white',
                 borderRadius: 3,
+                textAlign: 'center',
               }}>
               no languages yet!
             </Typography>
           )}
         </Card>
+
         <Card
           sx={{
             background: '#2f2f2f',
@@ -102,29 +139,52 @@ export default function SingleGuide() {
             width: '25%',
             minHeight: '10vh',
             textOverflow: 'ellipsis',
+            border: 1.25,
+            borderColor: '#353540',
           }}>
           <Typography sx={{ color: 'white' }} gutterBottom>
             Front End
           </Typography>
-          {frontEnd.map((item, idx) => {
-            return (
-              <Typography
-                key={idx}
-                sx={{
-                  color: 'white',
-                  fontSize: '0.7em',
-                  p: 0.5,
-                  mt: 0.5,
-                  mb: 0.5,
-                  border: 1,
-                  borderColor: 'white',
-                  borderRadius: 3,
-                }}>
-                {`${item.frontEnd}`}
-              </Typography>
-            )
-          })}
+          {frontEnd.length ? (
+            frontEnd.map((item, idx) => {
+              return (
+                <Typography
+                  key={idx}
+                  sx={{
+                    color: 'white',
+                    fontSize: '0.7em',
+                    minHeight: 18,
+                    p: 0.5,
+                    mt: 0.5,
+                    mb: 0.5,
+                    border: 1,
+                    borderColor: 'white',
+                    borderRadius: 3,
+                    textAlign: 'center',
+                  }}>
+                  {`${item}`}
+                </Typography>
+              )
+            })
+          ) : (
+            <Typography
+              sx={{
+                color: 'white',
+                fontSize: '0.7em',
+                minHeight: 18,
+                p: 0.5,
+                mt: 0.5,
+                mb: 0.5,
+                border: 1,
+                borderColor: 'white',
+                borderRadius: 3,
+                textAlign: 'center',
+              }}>
+              no front end yet!
+            </Typography>
+          )}
         </Card>
+
         <Card
           sx={{
             background: '#2f2f2f',
@@ -136,29 +196,52 @@ export default function SingleGuide() {
             width: '25%',
             minHeight: '10vh',
             textOverflow: 'ellipsis',
+            border: 1.25,
+            borderColor: '#353540',
           }}>
           <Typography sx={{ color: 'white' }} gutterBottom>
             Back End
           </Typography>
-          {backEnd.map((item, idx) => {
-            return (
-              <Typography
-                key={idx}
-                sx={{
-                  color: 'white',
-                  fontSize: '0.7em',
-                  p: 0.5,
-                  mt: 0.5,
-                  mb: 0.5,
-                  border: 1,
-                  borderColor: 'white',
-                  borderRadius: 3,
-                }}>
-                {`${item.backEnd}`}
-              </Typography>
-            )
-          })}
+          {backEnd.length ? (
+            backEnd.map((item, idx) => {
+              return (
+                <Typography
+                  key={idx}
+                  sx={{
+                    color: 'white',
+                    fontSize: '0.7em',
+                    minHeight: 18,
+                    p: 0.5,
+                    mt: 0.5,
+                    mb: 0.5,
+                    border: 1,
+                    borderColor: 'white',
+                    borderRadius: 3,
+                    textAlign: 'center',
+                  }}>
+                  {`${item.backEnd}`}
+                </Typography>
+              )
+            })
+          ) : (
+            <Typography
+              sx={{
+                color: 'white',
+                fontSize: '0.7em',
+                minHeight: 18,
+                p: 0.5,
+                mt: 0.5,
+                mb: 0.5,
+                border: 1,
+                borderColor: 'white',
+                borderRadius: 3,
+                textAlign: 'center',
+              }}>
+              no back end yet!
+            </Typography>
+          )}
         </Card>
+
         <Card
           sx={{
             background: '#2f2f2f',
@@ -170,57 +253,108 @@ export default function SingleGuide() {
             width: '25%',
             minHeight: '10vh',
             textOverflow: 'ellipsis',
+            border: 1.25,
+            borderColor: '#353540',
           }}>
           <Typography sx={{ color: 'white' }} gutterBottom>
             APIs
           </Typography>
-          {apis.map((item, idx) => {
-            return (
-              <Typography
-                key={idx}
-                sx={{
-                  color: 'white',
-                  fontSize: '0.7em',
-                  p: 0.5,
-                  mt: 0.5,
-                  mb: 0.5,
-                  border: 1,
-                  borderColor: 'white',
-                  borderRadius: 3,
-                }}>
-                {`${item.API}`}
-              </Typography>
-            )
-          })}
+          {apis ? (
+            apis.map((item, idx) => {
+              return (
+                <Typography
+                  key={idx}
+                  sx={{
+                    color: 'white',
+                    fontSize: '0.7em',
+                    minHeight: 18,
+                    p: 0.5,
+                    mt: 0.5,
+                    mb: 0.5,
+                    border: 1,
+                    borderColor: 'white',
+                    borderRadius: 3,
+                    textAlign: 'center',
+                  }}>
+                  {`${item.API}`}
+                </Typography>
+              )
+            })
+          ) : (
+            <Typography
+              sx={{
+                color: 'white',
+                fontSize: '0.7em',
+                minHeight: 18,
+                p: 0.5,
+                mt: 0.5,
+                mb: 0.5,
+                border: 1,
+                borderColor: 'white',
+                borderRadius: 3,
+                textAlign: 'center',
+              }}>
+              no APIs yet!
+            </Typography>
+          )}
         </Card>
       </Box>
+
+      {/*
+          start the tags here
+      */}
+
+      <Typography sx={{ color: 'white', mt: 1, ml: 1 }}>Other Tags</Typography>
       <Card
         sx={{
           background: '#2f2f2f',
-          mt: 2,
+          // mt: 0,
+          border: 1.25,
+          borderColor: '#353540',
         }}>
-        <Typography sx={{ color: 'white' }} gutterBottom>
-          Other Tags
-        </Typography>
-        <Box sx={{ display: 'flex', mb: 1 }}>
-          {tags.map((item, idx) => {
-            return (
-              <Typography
-                key={idx}
-                sx={{
-                  color: 'white',
-                  fontSize: '0.7em',
-                  border: 1,
-                  borderColor: 'white',
-                  borderRadius: 3,
-                  p: 0.5,
-                  ml: 1,
-                  mr: 1,
-                }}>
-                {`${item.tag}`}
-              </Typography>
-            )
-          })}
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', mb: 1 }}>
+          {tags.length ? (
+            tags.map((item, idx) => {
+              return (
+                <Typography
+                  key={idx}
+                  sx={{
+                    color: 'white',
+                    fontSize: '0.7em',
+                    minHeight: 18,
+                    border: 1,
+                    borderColor: 'white',
+                    borderRadius: 3,
+                    minWidth: 20,
+                    p: 0.5,
+                    mt: 0.5,
+                    mb: 0.5,
+                    ml: 1,
+                    mr: 1,
+                    alignItems: 'center',
+                    textAlign: 'center',
+                  }}>
+                  {`${item.tag}`}
+                </Typography>
+              )
+            })
+          ) : (
+            <Typography
+              sx={{
+                color: 'white',
+                fontSize: '0.7em',
+                minHeight: 18,
+                p: 0.5,
+                mt: 0.5,
+                mb: 0.5,
+                border: 1,
+                borderColor: 'white',
+                borderRadius: 3,
+                textAlign: 'center',
+              }}>
+              no tags yet!
+            </Typography>
+          )}
         </Box>
       </Card>
     </Container>
