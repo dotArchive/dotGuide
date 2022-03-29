f0d523034c29411a9bbe502ec7e4624edc8054f4
JollyRen <jollyren@gmail.com>
2022-03-29 20:42:15 +0200

    initial commit: navbar, footer, home, guidePreview fixes for color etc.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React from 'react'
+import { Card, Container } from '@mui/material/'
 
 const Footer = () => {
-	return (
-		<div style={{ backgroundColor: 'orange' }} id="footer">
-			<p>
-				Web Design & Development by{' '}
-				<a
-					href="https://github.com/dotArchive/dotGuide"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{`<dotGuide />`}
-				</a>
-			</p>
-		</div>
-	);
-};
+  return (
+    <Container sx={{ display: 'flex', justifyContent: 'center', mt: 1 }}>
+      <Card
+        sx={{ width: '52.5%', textAlign: 'center', borderRadius: 5 }}
+        style={{ backgroundColor: '#2f2f2f' }}
+        id="footer">
+        <p style={{ color: 'white' }}>
+          Web Design & Development by{' '}
+          <a
+            style={{ color: '#f57c00' }}
+            href="https://github.com/dotArchive/dotGuide"
+            target="_blank"
+            rel="noopener noreferrer">
+            {`<dotGuide />`}
+          </a>
+        </p>
+      </Card>
+    </Container>
+  )
+}
 
-export default Footer;
+export default Footer
