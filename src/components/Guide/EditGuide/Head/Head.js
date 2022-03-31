07ab59456cf010050861069be84f73518d7c1eaf
JollyRen <jollyren@gmail.com>
2022-03-31 23:02:17 +0200

    home page guides search all guides, styled search bar and buttons, added all guides button, fixed guides showing title.

--- src/components/Guide/EditGuide/Head/Head.js
+++ src/components/Guide/EditGuide/Head/Head.js
@@ -1,6 +1,15 @@
+// React Imports
 import React, { useEffect, useState } from 'react'
+
+// Firebase imports
 import { doc, updateDoc } from 'firebase/firestore'
 import { db } from '../../../../firebase'
+
+//MUI
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 
 // Components
 import BackEnd from './TechStack/BackEnd'
@@ -12,57 +21,25 @@ import Tag from './Tag'
 import GuideDescription from './GuideDescription'
 import CodeURL from './CodeURL'
 
-//MUI
-import { Typography, Box, IconButton, Card, Container } from '@mui/material'
-import BookmarkRoundedIcon from '@mui/icons-material/BookmarkRounded'
-import BookmarkBorderRoundedIcon from '@mui/icons-material/BookmarkBorderRounded'
-import ModeEditSharpIcon from '@mui/icons-material/ModeEditSharp'
-
 export default function Head(props) {
+  //document title and description
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
-  const [tags, setTags] = useState([])
-  const [urls, setUrl] = useState([])
-  const [apis, setApi] = useState([])
+  // head top cards
+  const [languages, setLanguages] = useState([])
   const [frontEnds, setFrontEnd] = useState([])
   const [backEnds, setBackEnd] = useState([])
-  const [languages, setLanguages] = useState([])
-  const [isFavorite, setIsFavorite] = useState(false)
+  const [apis, setApi] = useState([])
+  // head bottom cards
+  const [tags, setTags] = useState([])
+  const [urls, setUrl] = useState([])
 
   useEffect(() => {
     if (props.save === true) updateHead()
     if (props.submit === true) updateHead()
   })
 
-  let search = []
-  const guideId = props.guideId
-
-  const language = languages.map((language) => {
-    search.push(language.language)
-    return language
-  })
-  const tag = tags.map((tag) => {
-    search.push(tag.tag)
-    return tag
-  })
-  const url = urls.map((url) => {
-    return url
-  })
-
-  const API = apis.map((API) => {
-    search.push(API.API)
-    return API
-  })
-
-  const frontEnd = frontEnds.map((frontEnd) => {
-    search.push(frontEnd.frontEnd)
-    return frontEnd
-  })
-  const backEnd = backEnds.map((backEnd) => {
-    search.push(backEnd.backEnd)
-    return backEnd
-  })
-
+  //update the edited doc when ready
   const updateHead = async () => {
     search.push(title)
     const guideRef = doc(db, 'guides', guideId)
@@ -80,137 +57,134 @@ export default function Head(props) {
       search,
     })
   }
+  let search = []
+  const guideId = props.guideId
+
+  //Head top cards
+  const language = languages.map((language) => {
+    search.push(language.language)
+    return language
+  })
+  const API = apis.map((API) => {
+    search.push(API.API)
+    return API
+  })
+  const frontEnd = frontEnds.map((frontEnd) => {
+    search.push(frontEnd.frontEnd)
+    return frontEnd
+  })
+  const backEnd = backEnds.map((backEnd) => {
+    search.push(backEnd.backEnd)
+    return backEnd
+  })
+
+  //head bottom cards
+  const tag = tags.map((tag) => {
+    search.push(tag.tag)
+    return tag
+  })
+  const url = urls.map((url) => url)
+
+  //
+  const outerContainer = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  }
+  const topCard = {
+    background: '#2f2f2f',
+    p: 1,
+    pl: 2,
+    pr: 2,
+    border: 1.25,
+    borderColor: '#353540',
+    mb: '0.5rem',
+  }
+  const usernameTypography = {
+    color: 'white',
+    fontSize: '0.75em',
+    height: 40,
+  }
+  const techBox = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '10px',
+  }
+  const techOuterCard = {
+    background: '#2f2f2f',
+    p: 1,
+    pl: 2,
+    pr: 2,
+    width: '25%',
+    minHeight: '10vh',
+    textOverflow: 'ellipsis',
+    border: 1.25,
+    borderColor: '#353540',
+    flexGrow: 1,
+  }
+  const techCards = {
+    background: '#2f2f2f',
+    p: 1,
+    pl: 2,
+    pr: 2,
+    width: '25%',
+    minHeight: '10vh',
+    textOverflow: 'ellipsis',
+    border: 1.25,
+    borderColor: '#353540',
+    flexGrow: 1,
+  }
+  const tagUrlBox = {
+    display: 'flex',
+    mb: 1,
+    justifyContent: 'space-between',
+    gap: '10px',
+  }
+  const tagsUrlsCards = {
+    background: '#2f2f2f',
+    border: 1.25,
+    borderColor: '#353540',
+    flexGrow: 1,
+    p: 1,
+    pl: 2,
+    pr: 2,
+    width: '50%',
+  }
 
   return (
-    <Container
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignContent: 'center',
-        justifyContent: 'center',
-        color: 'white',
-      }}>
-      {/* {console.log(props)} */}
+    <Container sx={outerContainer}>
       <Title guide={props.guide} titleChild={(data) => setTitle(data)} />
-      <Card
-        sx={{
-          background: '#2f2f2f',
-          p: 1,
-          pl: 2,
-          pr: 2,
-          border: 1.25,
-          borderColor: '#353540',
-          mb: '0.5rem',
-        }}>
+      <Card sx={topCard}>
         <div className="flexbox" style={{ justifyContent: 'space-between' }}>
-          <Typography sx={{ color: 'white', fontSize: '0.75em', height: 40 }}>
-            {props.username}
-          </Typography>
+          <Typography sx={usernameTypography}>{props.username}</Typography>
         </div>
         <GuideDescription guide={props.guide} descriptionChild={(data) => setDescription(data)} />
       </Card>
       <Typography sx={{ color: 'white', ml: 1 }}></Typography>
 
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          gap: '10px',
-        }}>
-        <Card
-          sx={{
-            background: '#2f2f2f',
-            p: 1,
-            pl: 2,
-            pr: 2,
-            width: '25%',
-            minHeight: '10vh',
-            textOverflow: 'ellipsis',
-            border: 1.25,
-            borderColor: '#353540',
-            flexGrow: 1,
-          }}>
+      <Box sx={techBox}>
+        <Card sx={techOuterCard}>
           <Language guide={props.guide} languageChild={(data) => setLanguages(data)} />
         </Card>
-        <Card
-          sx={{
-            background: '#2f2f2f',
-            p: 1,
-            pl: 2,
-            pr: 2,
-            width: '25%',
-            minHeight: '10vh',
-            textOverflow: 'ellipsis',
-            border: 1.25,
-            borderColor: '#353540',
-            flexGrow: 1,
-          }}>
+        <Card sx={techCards}>
           <FrontEnd guide={props.guide} frontEndChild={(data) => setFrontEnd(data)} />
         </Card>
-        <Card
-          sx={{
-            background: '#2f2f2f',
-            p: 1,
-            pl: 2,
-            pr: 2,
-            width: '25%',
-            minHeight: '10vh',
-            textOverflow: 'ellipsis',
-            border: 1.25,
-            borderColor: '#353540',
-            flexGrow: 1,
-          }}>
+        <Card sx={techCards}>
           <BackEnd guide={props.guide} backEndChild={(data) => setBackEnd(data)} />
         </Card>
-        <Card
-          sx={{
-            background: '#2f2f2f',
-            p: 1,
-            pl: 2,
-            pr: 2,
-            width: '25%',
-            minHeight: '10vh',
-            textOverflow: 'ellipsis',
-            border: 1.25,
-            borderColor: '#353540',
-            flexGrow: 1,
-          }}>
+        <Card sx={techCards}>
           <Api guide={props.guide} apiChild={(data) => setApi(data)} />
         </Card>
       </Box>
 
       <Typography sx={{ color: 'white', mt: 1, ml: 1 }}></Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          mb: 1,
-          justifyContent: 'space-between',
-          gap: '10px',
-        }}>
-        <Card
-          sx={{
-            background: '#2f2f2f',
-            border: 1.25,
-            borderColor: '#353540',
-            flexGrow: 1,
-            p: 1,
-            pl: 2,
-            pr: 2,
-            width: '50%',
-          }}>
+      <Box sx={tagUrlBox}>
+        <Card sx={tagsUrlsCards}>
           <Tag guide={props.guide} tagChild={(data) => setTags(data)} />
         </Card>
-        <Card
-          sx={{
-            background: '#2f2f2f',
-            border: 1.25,
-            borderColor: '#353540',
-            flexGrow: 1,
-            p: 1,
-            pl: 2,
-            pr: 2,
-            width: '50%',
-          }}>
+        <Card sx={tagsUrlsCards}>
           <CodeURL guide={props.guide} urlChild={(data) => setUrl(data)} />
         </Card>
       </Box>
